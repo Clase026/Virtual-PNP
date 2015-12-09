6b5fd26e4431fbdb6c67ca0710d6d3aa15b141bb
Clase026 <aclasen.ac@gmail.com>
2015-12-08 19:14:27 -0600

    Prepared app for Heroku

--- chat.py
+++ chat.py
@@ -4,5 +4,5 @@ from app import create_app, socketio
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    #socketio.run(app,'http://safe-refuge-1607.herokuapp.com')
-    socketio.run(app)
+    socketio.run(app,'http://safe-refuge-1607.herokuapp.com')
+    #socketio.run(app)
