dc1ef587df36822732380bc84c22e0196a01a583
Clase026 <aclasen.ac@gmail.com>
2015-12-10 03:19:44 -0600

    removed namespaces

--- app/main/events.py
+++ app/main/events.py
@@ -3,7 +3,7 @@ from flask.ext.socketio import emit, join_room, leave_room
 from .. import socketio
 from . import pnpparser
 
-@socketio.on('joined', namespace='/')
+@socketio.on('joined')
 def joined(message):
     """Sent by clients when they enter a room.
     A status message is broadcast to all people in the room."""
@@ -12,7 +12,7 @@ def joined(message):
     emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
 
 
-@socketio.on('text', namespace='/')
+@socketio.on('text')
 def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
@@ -21,7 +21,7 @@ def text(message):
     emit('message', {'msg': session.get('name') + ': ' + output}, room=room)
 
 
-@socketio.on('left', namespace='/')
+@socketio.on('left')
 def left(message):
     """Sent by clients when they leave a room.
     A status message is broadcast to all people in the room."""
